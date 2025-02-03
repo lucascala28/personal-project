553e70a08259dd4bc6f89e2c229cf0605981133b
Luca Scala <lucascala@Lucas-MacBook-Pro.local>
2025-02-03 15:26:38 +0000

    Text Changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,10 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const name = "Luca Scala"
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>{name}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
