bf477926d7c24c81f8dea48c76281db143e8e738
Luca Scala <lucascala@Lucas-MacBook-Pro.local>
2025-02-04 18:33:01 +0000

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Projects from './pages/Projects';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
   const name = "Luca Scala"
   return (
-    <>
-      <h1 style={{ color: "white" }}>{name}</h1>
-     <p className="read-the-docs">Euefffff</p>
-    </>
+    <Router>
+      <Routes>
+          <Route path="/" element={<Home />} />
+	  <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
   )
 }
 
