4ca19232a128b175c1b51e80539dfd64421429f1
Luca Scala <lucascala@Lucas-MacBook-Pro.local>
2025-02-04 20:09:25 +0000

    create header for home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
 import Projects from './pages/Projects';
 import './App.css'
 
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+const Home = () => {
+  return (
+    <div className="box">
+      <h1 style={{color: '#fd23c1'}}>LUCA</h1>
+      <h1 style={{color: '#fd23c1'}}>SCALA</h1>
+      <p style={{textAlign: 'right', color: '#fd23c1'}}>Software Engineer</p>
+    </div>
+  );
+};
+
+export default Home;
